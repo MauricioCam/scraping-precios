470ed94c55faad8d3f1f15ba59f5328b99010ca4
MauricioCam <88343491+MauricioCam@users.noreply.github.com>
2025-07-27 10:43:07 -0300

    Delete productos_coto.py
    borrar este para cambiar ubicación

--- productos_coto.py
+++
@@ -1,5 +0,0 @@
-coto_urls = [
-    "https://www.cotodigital.com.ar/sitios/cdigi/productos/galletitas-oreo-rellenas-con-crema-sabor-original-354g/_/R-00532300-00532300-200?Dy=1&idSucursal=200",
-    "https://www.cotodigital.com.ar/sitios/cdigi/productos/alfajor-oreo-triple-56g/_/R-00524247-00524247-200?Dy=1&idSucursal=200",
-    "https://www.cotodigital.com.ar/sitios/cdigi/productos/galletitas-oreo-golden-sabor-vainilla-rellenas-con-crema-118g/_/R-00515725-00515725-200?Dy=1&idSucursal=200"
-]
